0c0e4ca152d8dfa12650d7043ef589af4ba97832
Akos Kitta <1405703+kittaakos@users.noreply.github.com>
2018-06-08 13:34:13 +0200

    Adjusted the response message.
    Signed-off-by: Akos Kitta <kittaakos@gmail.com>

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const express = require('express');
 
 const app = express();
-app.get('/', (req, res) => res.send('Hello Gitpod!'));
+app.get('/', (req, res) => res.send('Hello World, here is Gitpod!'));
 app.listen(4000, () => console.log('Example app is listening on port 4000.'));
